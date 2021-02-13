5846e22963c239c41a6d8dc860eca1edc8f0d96d
sparkle <discussionforall@gmail.com>
2021-02-13 14:47:07 +0530

    working on API validtion update

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -33,109 +33,130 @@ exports.createUser = async (req:Request, res:Response) => {
 
 exports.getUser = async (req:Request, res:Response) => {
 	const userRepository = getMongoRepository(User);
-    const getUser =await userRepository.find(ObjectID(req.params.id));
-    if(getUser.length > 0){
-    	res.status(200).send(getUser);
-    }else{
-    	res.status(204).send({message : "User not found"});
-    }
+	if(ObjectID.isValid(req.params.id)){
+	    const getUser =await userRepository.find(ObjectID(req.params.id));
+	    if(getUser.length > 0){
+	    	res.status(200).send(getUser);
+	    }else{
+	    	res.status(204).send({message : "User not found"});
+	    }
+	}else{
+		res.status(422).send({message : "Parameter missing or invalid"});
+	}
 }
 
 exports.updateUser = async(req:Request,res:Response)=>{
-	const userRepository = getMongoRepository(User);
-	req.body.updatedAt = new Date();
-    await userRepository.update({ 'id': ObjectID(req.params.id) },req.body)
-	res.status(200).send(req.body);	
+	if(ObjectID.isValid(req.params.id)){
+		const userRepository = getMongoRepository(User);
+		req.body.updatedAt = new Date();
+	    await userRepository.update({ 'id': ObjectID(req.params.id) },req.body)
+		res.status(200).send(req.body);
+	}else{
+		res.status(422).send({message : "Parameter missing or invalid"});
+	}
+		
 }
 
 exports.addUsdInUser = async (req:Request,res:Response)=>{
 	const userRepository = getMongoRepository(User);
-	const getUser = await userRepository.find(ObjectID(req.params.id));
-	if(getUser.length > 0){
-		if(req.body.action && req.body.action != " " 
-			&&  req.body.amount && req.body.amount != " "){
-			if(req.body.amount > 0){
-				if(req.body.action == "withdraw"){
-					if(Number(req.body.amount) < Number(getUser[0].usdBalance)){
+	if(ObjectID.isValid(req.params.id)){
+		const getUser = await userRepository.find(ObjectID(req.params.id));
+		if(getUser.length > 0){
+			if(req.body.action && req.body.action != " " 
+				&&  req.body.amount && req.body.amount != " "){
+				if(req.body.amount > 0){
+					if(req.body.action == "withdraw"){
+						if(Number(req.body.amount) < Number(getUser[0].usdBalance)){
+							let reqObject = {
+								usdBalance :  Number(getUser[0].usdBalance) - Number(req.body.amount) 
+							}
+							await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
+							res.status(200).send(req.body);
+						}else{
+							res.status(422).send({message : "Amount is not withdrawable"});
+						}
+					}else if(req.body.action == "deposit"){
 						let reqObject = {
-							usdBalance :  Number(getUser[0].usdBalance) - Number(req.body.amount) 
+							usdBalance : Number(req.body.amount) + Number(getUser[0].usdBalance)
 						}
 						await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
 						res.status(200).send(req.body);
 					}else{
-						res.status(422).send({message : "Amount is not withdrawable"});
+						res.status(422).send({message : "Some value are missign"});
 					}
-				}else if(req.body.action == "deposit"){
-					let reqObject = {
-						usdBalance : Number(req.body.amount) + Number(getUser[0].usdBalance)
-					}
-					await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
-					res.status(200).send(req.body);
 				}else{
-					res.status(422).send({message : "Some value are missign"});
+					res.status(422).send({message : "Negative amount can't be "+ req.body.action});
 				}
+				
 			}else{
-				res.status(422).send({message : "Negative amount can't be "+ req.body.action});
+				res.status(422).send({message : "Some value are missign"});
 			}
-			
 		}else{
-			res.status(422).send({message : "Some value are missign"});
+			res.status(422).send({message : "User not found"});
 		}
 	}else{
-		res.status(422).send({message : "User not found"});
+		res.status(422).send({message : "Parameter missing or invalid"});
 	}
 }
 
 exports.addBitcoinsInUser =async (req:Request,res:Response)=>{
 	const userRepository = getMongoRepository(User);
-	const getUser = await userRepository.find(ObjectID(req.params.id));
-	if(getUser.length > 0){
-		if(req.body.action && req.body.action != " " 
-			&&  req.body.amount && req.body.amount != " "){
-			if(req.body.amount > 0){
-				if(req.body.action == "buy"){
-					let reqObject = {
-						bitcoinAmount : Number(req.body.amount) + Number(getUser[0].bitcoinAmount)
-					}
-					await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
-					res.status(200).send(req.body);
-				}else if(req.body.action == "sell"){
-					if(Number(req.body.amount) < Number(getUser[0].bitcoinAmount)){
+	if(ObjectID.isValid(req.params.id)){
+		const getUser = await userRepository.find(ObjectID(req.params.id));
+		if(getUser.length > 0){
+			if(req.body.action && req.body.action != " " 
+				&&  req.body.amount && req.body.amount != " "){
+				if(req.body.amount > 0){
+					if(req.body.action == "buy"){
 						let reqObject = {
-							bitcoinAmount :  Number(getUser[0].bitcoinAmount) - Number(req.body.amount) 
+							bitcoinAmount : Number(req.body.amount) + Number(getUser[0].bitcoinAmount)
 						}
 						await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
 						res.status(200).send(req.body);
+					}else if(req.body.action == "sell"){
+						if(Number(req.body.amount) < Number(getUser[0].bitcoinAmount)){
+							let reqObject = {
+								bitcoinAmount :  Number(getUser[0].bitcoinAmount) - Number(req.body.amount) 
+							}
+							await userRepository.update({ 'id': ObjectID(req.params.id) },reqObject)
+							res.status(200).send(req.body);
+						}else{
+							res.status(422).send({message : "You have not enough bitcoin for sell"});
+						}
 					}else{
-						res.status(422).send({message : "You have not enough bitcoin for sell"});
+						res.status(422).send({message : "Some value are missign"});
 					}
 				}else{
-					res.status(422).send({message : "Some value are missign"});
+					res.status(422).send({message : "Amount can't be negative"});
 				}
 			}else{
-				res.status(422).send({message : "Amount can't be negative"});
+				res.status(422).send({message : "Some value are missign"});
 			}
 		}else{
-			res.status(422).send({message : "Some value are missign"});
+			res.status(422).send({message : "User not found"});
 		}
 	}else{
-		res.status(422).send({message : "User not found"});
+		res.status(422).send({message : "Parameter missing or invalid"});
 	}
 }
 
 exports.userBalance = async(req:Request,res:Response)=>{
 	const userRepository = getMongoRepository(User);
 	const settingsRepository = getMongoRepository(Settings);
-	const getUser = await userRepository.find(ObjectID(req.params.id));
-	const settingObject = await settingsRepository.findOne(1);
-	if(getUser.length > 0 && settingObject){
-		let bitcoinbalance = Number(settingObject.price) * Number(getUser[0].bitcoinAmount);
-		bitcoinbalance = Number(bitcoinbalance.toFixed(0));
-		let resObject = {
-			balance :   Number(getUser[0].usdBalance) + Number(bitcoinbalance)
+	if(ObjectID.isValid(req.params.id)){
+		const getUser = await userRepository.find(ObjectID(req.params.id));
+		const settingObject = await settingsRepository.findOne(1);
+		if(getUser.length > 0 && settingObject){
+			let bitcoinbalance = Number(settingObject.price) * Number(getUser[0].bitcoinAmount);
+			bitcoinbalance = Number(bitcoinbalance.toFixed(0));
+			let resObject = {
+				balance :   Number(getUser[0].usdBalance) + Number(bitcoinbalance)
+			}
+			res.status(200).send(resObject);
+		}else{
+			res.status(422).send({message : "Information not found"});
 		}
-		res.status(200).send(resObject);
 	}else{
-		res.status(422).send({message : "Information not found"});
+		res.status(422).send({message : "Parameter missing or invalid"});
 	}
 }
